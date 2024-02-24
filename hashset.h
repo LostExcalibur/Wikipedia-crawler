3b0443c55fbaa50976d81473ab80598e74d6828e
LostExcalibur <romaindebeaucorps@yahoo.fr>
2024-02-24 17:16:26 +0100

    Do not expose bucket specific functions in hader

--- hashset.h
+++ hashset.h
@@ -16,11 +16,9 @@ typedef struct {
     struct bucket *buckets[HASHSET_NUM_BUCKETS];
 } hashset_t;
 
-struct bucket *new_bucket();
 hashset_t new_hashset();
 void hashset_insert(hashset_t *hashset, const char *key);
 bool hashset_search(hashset_t *hashset, const char *key);
-void delete_bucket(struct bucket *bucket);
 void delete_hashset(hashset_t *hashset);
 
 #endif // __HASHSET_H
